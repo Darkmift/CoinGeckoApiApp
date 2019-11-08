7132fbb741049f9fcc19b39292a131b0aca9714c
dark_mift <darkmift@gmail.com>
2019-11-09 01:31:02 +0200

    added css & js for hamburger menu visual

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-$('.navbar-nav .nav-link').click(function(){
-  $('.navbar-nav .nav-link').removeClass('active');
-  $(this).addClass('active');
+//toggle active css
+$('.navbar-nav .nav-link').click(function() {
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
 })
+
+//fix css for hamburger menu
+$(window).resize(function() {
+    windowsize = $(window).width();
+    if (windowsize > 990) {
+        $(".nav-link")
+            .removeClass('nav-item-small bg-dark')
+            .css('margin:0;')
+    }
+});
+
+$(".navbar-toggler-icon").click(function() {
+    windowsize = $(window).width();
+    if (windowsize < 990) {
+        $(".nav-link")
+            .addClass('nav-item-small bg-dark')
+    }
+});
+//fix css for hamburger menu -end
